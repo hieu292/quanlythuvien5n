392d477b234a98872e3c17a39ca2afa828d5cfdf
LNHnam <LNHnam@gmail.com>
2009-04-17 17:18:30 +0000

    Code ChiTietPhieuMuonDao,PhieuMuonDao

--- Source Code/QuanLyThuVien/DAO/DataProvider.cs
+++ Source Code/QuanLyThuVien/DAO/DataProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.OleDb;
 
 namespace DAO
 {
--- Source Code/QuanLyThuVien/DAO/NgonNguDao.cs
+++ Source Code/QuanLyThuVien/DAO/NgonNguDao.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
 using System.Text;
+using DTO;
 
 namespace DAO
 {
     public class NgonNguDao
     {
+        
     }
 }
