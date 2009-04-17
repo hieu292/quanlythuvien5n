2a4d663ef82cf83336f32fea41c1e46b8a85211b
LNHnam@gmail.com <LNHnam@gmail.com@4799ef50-179c-11de-a705-0f8aa5a5abf1>
2009-04-17 17:18:30 +0000

    Code ChiTietPhieuMuonDao,PhieuMuonDao

--- trunk/Source Code/QuanLyThuVien/DTO/PhieuMuonDto.cs
+++ trunk/Source Code/QuanLyThuVien/DTO/PhieuMuonDto.cs
@@ -7,7 +7,7 @@ namespace DTO
     public class PhieuMuonDto
     {
         #region Atributes
-        private int mPhieu;        
+        private int mPhieu;
         private int mDocGia;
         private DateTime ngayMuon;
         private int soNgayMuon;
