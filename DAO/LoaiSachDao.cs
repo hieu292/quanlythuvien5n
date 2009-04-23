85c63bd8cbd6d8e6c554bea8004f6f994eff8bbf
vqnhat <vqnhat@4799ef50-179c-11de-a705-0f8aa5a5abf1>
2009-04-23 16:41:04 +0000

    Sua doi cau truc thu muc

---
+++ DAO/LoaiSachDao.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Text;
+using DTO;
+
+namespace DAO
+{
+    public class LoaiSachDao
+    {
+        public static DataTable GetTable()
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From LoaiSach";
+            OleDbDataAdapter da;
+            da = new OleDbDataAdapter(strSQL, cn);
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+        }
+        public static IList GetList()
+        {
+            ArrayList ds = new ArrayList();
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From LoaiSach";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                LoaiSachDto loaiSach = new LoaiSachDto();
+                loaiSach.MLoaiSach = (int)dr["MLoaiSach"];
+                loaiSach.TenLoaiSach = (String)dr["TenLoaiSach"];                
+                ds.Add(loaiSach);
+            }
+            dr.Close();
+            cn.Close();
+            return ds;
+
+        }
+        public static LoaiSachDto Search(int maLoaiSach)
+        {
+            LoaiSachDto loaiSach = null;
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From LoaiSach Where MLoaiSach= ? ";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MLoaiSach", OleDbType.Integer);
+            cmd.Parameters["@MLoaiSach"].Value = maLoaiSach;
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                loaiSach = new LoaiSachDto();
+                loaiSach.MLoaiSach = (int)dr["MLoaiSach"];
+                loaiSach.TenLoaiSach = (String)dr["TenLoaiSach"];                
+            }
+            dr.Close();
+            cn.Close();
+            return loaiSach;
+        }
+        public static int Add(LoaiSachDto loaiSach)
+        {
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+
+            string strSQL;
+            strSQL = "Insert into LoaiSach(MLoaiSach,TenLoaiSach) values (?,?)";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MLoaiSach", OleDbType.Integer);
+            cmd.Parameters.Add("@TenLoaiSach", OleDbType.WChar);
+            cmd.Parameters["@MLoaiSach"].Value = loaiSach.MLoaiSach;
+            cmd.Parameters["@TenLoaiSach"].Value = loaiSach.TenLoaiSach;
+            
+            cmd.ExecuteNonQuery();
+            strSQL = "Select @@IDENTITY";
+            cmd = new OleDbCommand(strSQL, cn);
+            loaiSach.MLoaiSach = (int)cmd.ExecuteScalar();
+            cn.Close();
+            return loaiSach.MLoaiSach;
+        }
+        public static void Delete(int maLoaiSach)
+        {
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Delete From  LoaiSach Where MLoaiSach = ?";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MLoaiSach", OleDbType.Integer);
+            cmd.Parameters["@MLoaiSach"].Value = maLoaiSach;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+        public static void Update(LoaiSachDto loaiSach)
+        {
+            OleDbConnection cn;
+
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Update LoaiSach Set MLoaiSach = ? , TenLoaiSach = ? Where MLoaiSach = ?";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MLoaiSach", OleDbType.Integer);
+            cmd.Parameters.Add("@TenLoaiSach", OleDbType.WChar);
+            
+            cmd.Parameters["@MLoaiSach"].Value = loaiSach.MLoaiSach;
+            cmd.Parameters["@TenLoaiSach"].Value = loaiSach.TenLoaiSach;
+            
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+    }
+}
---
+++ DAO/PhieuMuonDao.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Text;
+using DTO;
+
+namespace DAO
+{
+    public class PhieuMuonDao
+    {
+        public static DataTable GetTable()
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From PhieuMuon";
+            OleDbDataAdapter da;
+            da = new OleDbDataAdapter(strSQL, cn);
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+        }
+        public static IList GetList()
+        {
+            ArrayList ds = new ArrayList();
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From PhieuMuon";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                PhieuMuonDto pm = new PhieuMuonDto();
+                pm.MPhieu = (int)dr["MPhieu"];
+                pm.MDocGia = (int)dr["MDocGia"];
+                pm.NgayMuon = (DateTime)dr["NgayMuon"];
+                pm.SoNgayMuon = (int)dr["SoNgayMuon"];
+                ds.Add(pm);
+            }
+            dr.Close();
+            cn.Close();
+            return ds;
+
+        }
+        public static PhieuMuonDto Search(int MaPh)
+        {
+            PhieuMuonDto pm = null;
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Select * From PhieuMuon Where MPhieu= ? ";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MPhieu", OleDbType.Integer);
+            cmd.Parameters["@MPhieu"].Value = MaPh;
+            OleDbDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                pm = new PhieuMuonDto();
+                pm.MPhieu = (int)dr["MPhieu"];
+                pm.MDocGia = (int)dr["MDocGia"];
+                pm.NgayMuon = (DateTime)dr["NgayMuon"];
+                pm.SoNgayMuon = (int)dr["SoNgayMuon"];
+            }
+            dr.Close();
+            cn.Close();
+            return pm;
+        }
+        public static int Add(PhieuMuonDto pm)
+        {
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+
+            string strSQL;
+            strSQL = "Insert into PhieuMuon(MDocGia,NgayMuon,SoNgayMuon) values (?,?,?)";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MDocGia", OleDbType.Integer);
+            cmd.Parameters.Add("@NgayMuon", OleDbType.Date);
+            cmd.Parameters.Add("@SoNgayMuon", OleDbType.Integer);
+            cmd.Parameters["@MDocGia"].Value = pm.MDocGia;
+            cmd.Parameters["@NgayMuon"].Value = pm.NgayMuon;
+            cmd.Parameters["@SoNgayMuon"].Value = pm.SoNgayMuon;
+            
+            cmd.ExecuteNonQuery();
+            strSQL = "Select @@IDENTITY";
+            cmd = new OleDbCommand(strSQL, cn);
+            pm.MPhieu = (int)cmd.ExecuteScalar();
+            cn.Close();
+            return pm.MPhieu;
+        }
+        public static void Delete(int MaPh)
+        {
+            OleDbConnection cn;
+            cn = DataProvider.ConnectionData();
+            string strSQL;
+            strSQL = "Delete From  PhieuMuon Where MPhieu= ? ";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MPhieu", OleDbType.Integer);
+            cmd.Parameters["@MPhieu"].Value = MaPh;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+        public static void Update(PhieuMuonDto pm)
+        {
+            OleDbConnection cn;
+
+            cn = DataProvider.ConnectionData();
+
+            string strSQL;
+            strSQL = "Update PhieuMuon Set MDocGia = ? , NgayMuon = ? , SoNgayMuon = ? Where MPhieu = ?";
+            OleDbCommand cmd = new OleDbCommand(strSQL, cn);
+            cmd.Parameters.Add("@MDocGia", OleDbType.Integer);
+            cmd.Parameters.Add("@NgayMuon", OleDbType.Date);
+            cmd.Parameters.Add("@SoNgayMuon", OleDbType.Integer);
+            cmd.Parameters.Add("@MPhieu", OleDbType.Integer);
+
+            cmd.Parameters["@MDocGia"].Value = pm.MDocGia;
+            cmd.Parameters["@NgayMuon"].Value = pm.NgayMuon;
+            cmd.Parameters["@SoNgayMuon"].Value = pm.SoNgayMuon;
+            cmd.Parameters["@MPhieu"].Value = pm.MPhieu;
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+    }
+}
